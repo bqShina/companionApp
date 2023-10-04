df15d7e63c148ed4a609f67db54886c51f070639
anneelv <anneelvira.lauw@gmail.com>
2023-10-04 11:57:18 +1100

    Adjust light and dark mode for HomePage View

--- PraShina/HomePageView.swift
+++ PraShina/HomePageView.swift
@@ -8,137 +8,101 @@
 import SwiftUI
 
 struct HomePageView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        ZStack{
-            Color.black
-                .edgesIgnoringSafeArea(.all)
-            VStack {
-                HStack{
-                    Text("Level 1")
-                        .font(.system(size:30, weight: .bold))
-                        .foregroundColor(.white)
-                        .offset(y:20)
-                    Image(systemName: "trophy")
-                        .dynamicTypeSize(.xxxLarge)
-                        .foregroundColor(.white)
-                        .offset(y:20)
-                }
-                Text("Complete 3 tasks to level up!")
-                    .dynamicTypeSize(.xLarge)
-                    .foregroundColor(.white)
-                    .offset(y:30)
+       
+        VStack {
+            HStack{
+                Text("Level 1")
+                    .font(.system(size:30, weight: .bold))
+                    .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                    .offset(y:20)
+                Image(systemName: "trophy")
+                    .dynamicTypeSize(.xxxLarge)
+                    .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                    .offset(y:20)
+            }
+            Text("Complete 3 tasks to level up!")
+                .dynamicTypeSize(.xLarge)
+                .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                .offset(y:30)
                 
                 //            EVOLUTION BOX
-                ZStack{
-                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                        .frame(width:335, height:85)
-                        .foregroundColor(.white)
-                        .opacity(0.2)
-                        .offset(y:30)
-                    HStack{
+            ZStack{
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                    .frame(width:335, height:85)
+                    .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                    .opacity(0.2)
+                    .offset(y:30)
+                HStack{
                         //                    1st evolution
-                        Image("dogevoHome")
-                            .resizable()
-                            .frame(width:40, height: 40)
+                    Image("dogevoHome")
+                        .resizable()
+                        .frame(width:40, height: 40)
+                        .offset(y:30)
+                    VStack{
+                        Text("Lv. 10")
+                            .dynamicTypeSize(.xSmall)
+                            .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
                             .offset(y:30)
-                        VStack{
-                            Text("Lv. 10")
-                                .dynamicTypeSize(.xSmall)
-                                .foregroundColor(.white)
-                                .offset(y:30)
-                            Image(systemName: "arrowshape.right")
-                                .dynamicTypeSize(.xxLarge)
-                                .foregroundColor(.white)
-                                .offset(y:30)
-                        }
+                        Image(systemName: "arrowshape.right")
+                            .dynamicTypeSize(.xxLarge)
+                            .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                            .offset(y:30)
+                    }
                         
                         //                    2nd evolution
-                        Image("dogevoHome")
-                            .resizable()
-                            .opacity(0.4)
-                            .frame(width:60, height: 60)
+                    Image("dogevoHome")
+                        .resizable()
+                        .opacity(0.4)
+                        .frame(width:60, height: 60)
+                        .offset(y:30)
+                    VStack{
+                        Text("Lv. 20")
+                            .dynamicTypeSize(.xSmall)
+                            .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
                             .offset(y:30)
-                        VStack{
-                            Text("Lv. 20")
-                                .dynamicTypeSize(.xSmall)
-                                .foregroundColor(.white)
-                                .offset(y:30)
-                            Image(systemName: "arrowshape.right")
-                                .dynamicTypeSize(.xxLarge)
-                                .foregroundColor(.white)
-                                .offset(y:30)
-                        }
+                         Image(systemName: "arrowshape.right")
+                            .dynamicTypeSize(.xxLarge)
+                            .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                            .offset(y:30)
+                    }
                         
                         //                    3rd evolution
-                        Image("dogevoHome")
-                            .resizable()
-                            .opacity(0.4)
-                            .frame(width:80, height: 80)
-                            .offset(y:30)
+                    Image("dogevoHome")
+                        .resizable()
+                        .opacity(0.4)
+                        .frame(width:80, height: 80)
+                        .offset(y:30)
                         
-                    }
                 }
+            }
                 
                 //            Main dog animated picture
-                Image("dogHome")
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundStyle(.tint)
-                    .offset(y:30)
+            Image("dogHome")
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(.tint)
+                .offset(y:30)
                     
                 
-    //            Customization box
+    //        Customization box
+            
+            Button(){
                 
-                Button(){
-                    
-                }
-                label: {
-                    Text("Customize")
-                        .padding(.horizontal, 80)
-                        .padding(.vertical, 15)
-                        .font(.system(size:24, weight: .bold))
-                        .foregroundColor(.white)
-                        .background(.gray)
-                        .cornerRadius(40)
-//                       Image(systemName: "pencil.circle")
-                }
-                .offset(y:-45)
-                    
-                
-                
-                Divider()
-    //            Tab Menu
-                HStack{
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "pawprint")
-                            .opacity(0.4)
-                            .imageScale(.large)
-                            .foregroundColor(.white)
-                    }
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "checklist")
-                            .imageScale(.large)
-                            .foregroundColor(.white)
-                    }
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "calendar")
-                            .imageScale(.large)
-                            .foregroundColor(.white)
-                    }
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "gearshape")
-                            .imageScale(.large)
-                            .foregroundColor(.white)
-                    }
-                    Spacer()
-
-                }
-                
-                .padding()
             }
+            label: {
+                Label("Customize", systemImage:"pencil.circle")
+                    .padding(.horizontal, 80)
+                    .padding(.vertical, 15)
+                    .font(.system(size:24, weight:.bold))
+                    .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                    .background(.gray)
+                    .cornerRadius(40)
+                    
+            }
+            .offset(y:-45)
+                
         }
     }
 }
