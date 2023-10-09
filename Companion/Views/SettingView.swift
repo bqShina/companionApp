f248561d2cbab24622b2bca9dafea5084750960e
anneelv <anneelvira.lauw@gmail.com>
2023-10-09 13:34:07 +1100

    adjust the variable name

--- Companion/Views/SettingView.swift
+++ Companion/Views/SettingView.swift
@@ -23,7 +23,7 @@ struct SettingView: View {
             
             HStack(spacing: 80) {
                 Image("small_dog")
-                Text("Your companion")
+                Text("\(name)")
                     .font(.system(size: 18))
                     .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                     .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
