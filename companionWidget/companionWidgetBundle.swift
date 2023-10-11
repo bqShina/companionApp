26128fe873764fd2ee5653cb105d646fbe0464df
anneelv <anneelvira.lauw@gmail.com>
2023-10-11 11:00:18 +1100

    New Widget file

---
+++ companionWidget/companionWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  companionWidgetBundle.swift
+//  companionWidget
+//
+//  Created by Anne Elvira on 11/10/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct companionWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        companionWidget()
+        companionWidgetLiveActivity()
+    }
+}
